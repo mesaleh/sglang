ef86e8c60dd54daa1ac65a2f7042f3a85c0062f1
Moustafa Saleh <moustafa.saleh@omniva.com>
2026-05-17 16:17:46 -0700

    Point Phase65 launcher to fixed image

--- scripts/phase65_launch_prear_add_gb200.sh
+++ scripts/phase65_launch_prear_add_gb200.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-IMAGE="${IMAGE:-gitlab.aws.omniva.com:5050/acls/security-analytics/sglang:gb200-kimi-k26-phase65-prear-add-1bf8fe40d-20260517}"
+IMAGE="${IMAGE:-gitlab.aws.omniva.com:5050/acls/security-analytics/sglang:gb200-kimi-k26-phase65-prear-add-8d093ecaf-fix1-20260517}"
 NAME="${NAME:-kimi-gb200-best}"
 MODEL_DIR="${MODEL_DIR:-/opt/kimi-gb200/models/Kimi-K2.6-2755962d}"
 PORT="${PORT:-30000}"
